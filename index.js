613a49dcdac5fa24e2ab758f0518a832ad021d76
Marius <mariusionitabogdan@gmail.com>
2025-03-04 08:51:32 +0200

    update

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-
-
-const e = require('express');
-const axios = require('axios');
-
-
+const e = require("express");
+const axios = require("axios");
 
 const app = e();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/random";
 
-app.use(e.static('public/'));
+app.use(e.static("public/"));
 
 app.get("/", async (req, res) => {
-    try {
-        const result = await axios.get(API_URL);
-        res.render("index.ejs", {data: result.data});
-        
-    } catch {
-        res.status(404).send("Error");
-    }
-
-    res.render("index.ejs");
+  try {
+    const result = await axios.get(API_URL);
+    res.render("index.ejs", { data: result.data });
+  } catch {
+    res.render("index.ejs", { data: { secret: "No secret available", username: "Unknown User" } });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
